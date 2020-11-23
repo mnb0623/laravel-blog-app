03c34f58f47e462d773b77da008e9077d184c434
thinkpad_mnb062355 <boomboom0623@gmail.com>
2020-11-23 16:13:07 +0900

    #7 create blog seeder&blog factory

---
+++ database/seeders/BlogsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Blog;
+class BlogsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Blog::factory()->times(10)->create();
+    }
+}
