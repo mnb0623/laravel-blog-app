df90af93135030631891b6f836910540965ceecf
thinkpad_mnb062355 <boomboom0623@gmail.com>
2020-11-26 01:13:26 +0900

    #9 記事一覧画面にセッションにエラーが存在していたらエラーを表示する

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -4,6 +4,11 @@
   <div class="row">
     <div class="col-md-8 col-md-offset-2">
       <h2>ブログ記事一覧</h2>
+      @if (session('err_msg'))
+          <p class="text-danger">
+            {{ session('err_msg') }}
+          </p>
+      @endif
       <table class="table table-striped">
         <tr>
           <th>記事番号</th>
