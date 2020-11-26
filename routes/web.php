951a519ec64b1d8430c7de932c642e5ee0535a1e
thinkpad_mnb062355 <boomboom0623@gmail.com>
2020-11-26 21:29:49 +0900

    #10 ルーティングの作成

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\BlogController@showList')->name('blogs');
 // !動画とはlaravelのバージョンが異なり記法が変わってる
 // https://qiita.com/norichintnk/items/34a04cd17bfe4014313a
+// #10 ブログ登録画面を表示
+Route::get('/blog/create', 'App\Http\Controllers\BlogController@showCreate')->name('create');
+// #10 ブログ登録
+Route::get('/blog/store', 'App\Http\Controllers\BlogController@exeStore')->name('store');
 // #8 ブログ詳細画面を表示
 // #8 これも動画とはバージョンが違うので記法が異なる
 Route::get('/blog/{id}', 'App\Http\Controllers\BlogController@showDetail')->name('show');
