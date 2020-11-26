98441dba5287913287a60674909a403c9bc2bfe6
thinkpad_mnb062355 <boomboom0623@gmail.com>
2020-11-26 21:49:51 +0900

    #10 bugFix メソッド修正get>post

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', 'App\Http\Controllers\BlogController@showList')->name('blogs');
 // #10 ブログ登録画面を表示
 Route::get('/blog/create', 'App\Http\Controllers\BlogController@showCreate')->name('create');
 // #10 ブログ登録
-Route::get('/blog/store', 'App\Http\Controllers\BlogController@exeStore')->name('store');
+Route::post('/blog/store', 'App\Http\Controllers\BlogController@exeStore')->name('store');
 // #8 ブログ詳細画面を表示
 // #8 これも動画とはバージョンが違うので記法が異なる
 Route::get('/blog/{id}', 'App\Http\Controllers\BlogController@showDetail')->name('show');
