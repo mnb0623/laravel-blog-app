5680b9f91e64477b425c0d621ccf5516c1512512
thinkpad_mnb062355 <boomboom0623@gmail.com>
2020-11-29 01:43:22 +0900

    #11 ブログデータ削除機能作成

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -108,5 +108,25 @@ class BlogController extends Controller
         \Session::flash('err_msg', 'ブログを更新しました。');
         return redirect(route('blogs'));
     }
-
+        /** *
+     * ブログ削除
+     * @param int $id
+     * @return view
+     */
+    public function exeDelete($id)
+    {
+        if (empty($id)) {
+            \Session::flash('err_msg', 'データがありません。');
+            return redirect(route('blogs'));
+        }
+        try {
+            //ブログを削除
+            Blog::destroy($id);
+        } catch(\Throwable $e) {
+            \DB::rollback();
+        }
+        
+        \Session::flash('err_msg', '削除しました。');
+        return redirect(route('blogs'));
+    }
 }
